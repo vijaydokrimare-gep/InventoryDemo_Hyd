3b986475ae8e3d7aee7534ee180065cf8477e60f
vijaydokrimare420 <vijay.dokrimare@gep.com>
2020-02-28 13:17:27 +0530

    Initail react native project setup

---
+++ RNInventory_Demo_Hyd/android/app/src/main/java/com/rninventory_demo_hyd/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rninventory_demo_hyd;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RNInventory_Demo_Hyd";
+  }
+}
